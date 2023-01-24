38a1cf5addba1892f9b745864a55ccdd73c468c1
cristianacmc <cmdasilvacosta@gmail.com>
2023-01-24 15:14:54 +0000

    add cluster file

---
+++ create_cluster.sh
@@ -0,0 +1,12 @@
+eksctl create cluster \
+--name ClusterGit \
+--region eu-west-2 \
+--nodegroup-name DemoNodes \
+--nodes 8 \
+--nodes-min 2 \
+--nodes-max 10 \
+--node-type t3.medium \
+--with-oidc \
+--ssh-access \
+--ssh-public-key AMSkey \
+--managed
